d29b14ae4f2bdfc1253033b5444aa850d13e8548
bachx <b21062002@gmail.com>
2024-12-01 21:57:38 +0700

    add stripe account column to user table

---
+++ src/backend/MediaMarket.Infrastructure/Migrations/20241201090959_AddStripeAccountIdToUserTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaMarket.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStripeAccountIdToUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountId",
+                table: "Withdrawals",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StripeAccountId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Withdrawals");
+
+            migrationBuilder.DropColumn(
+                name: "StripeAccountId",
+                table: "Users");
+        }
+    }
+}
